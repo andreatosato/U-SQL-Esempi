ef5d16a3ad7a9919ca10a19f5dbe75ee61203cb8
Andrea Tosato <andrea.tosato@hotmail.it>
2017-12-30 11:22:27 +0100

    Esempi

---
+++ NationsLogic/Nations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NationsLogic
+{
+    public static class Nations
+    {
+        private static IEnumerable<string> EuroNations = new[] { "Italia", "Slovacchia", "Albania", "Francia", "Liechtenstein" };
+        public static bool IsEuropean(string nation)
+        {
+            if (EuroNations.Contains(nation))
+                return true;
+            return false;
+        }
+    }
+}
